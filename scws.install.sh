5c6ac55d7b77011bc51c2e9eefecf566afee73e9
李铭昕 <715557344@qq.com>
2018-03-14 17:29:44 +0800

    增加安装脚本

---
+++ scws.install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+version='1.2.3'
+prefix='/Users/limx/Applications/limingxinleo/docker-scws/scws'
+
+# 下载并解压
+wget http://www.xunsearch.com/scws/down/scws-${version}.tar.bz2
+tar xvjf scws-${version}.tar.bz2
+
+# 安装Scws
+cd scws-${version}
+./configure --prefix=${prefix} && \
+make && \
+make install
+
+# 建立软连接
+ln -sf ${prefix}/bin/scws /usr/local/bin/scws
+
+# 检查安装结果
+ls -al ${prefix}/lib/libscws.la
+scws -h
+
+
+
+
